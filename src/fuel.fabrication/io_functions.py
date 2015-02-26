58dcb246294a7de4fc8867f2bb360d060cad45d1
R. A. Borrelli <r.angelo.borrelli@gmail.com>
2015-02-25 16:27:52 -0800

    Update io_functions.py
    kmp update

--- src/fuel.fabrication/io_functions.py
+++ src/fuel.fabrication/io_functions.py
@@ -1,7 +1,7 @@
 ########################################################################
 # R.A.Borrelli
 # @TheDoctorRAB
-# rev.17.February.2015
+# rev.25.February.2015
 ########################################################################
 # 
 # Functions for in-simulation data processing
@@ -24,7 +24,6 @@ import os
 #
 ########################################################################
 ####### (b): Set facility configuration
-####### (e): KMP measurement
 ####### (f): melter
 ####### (g): trimmer
 ####### (h): product processing
@@ -95,23 +94,20 @@ def input_parameters(home_dir,input_dir,output_data_dir):
 ###
 #
 ### open data files
-    batch=numpy.loadtxt('batch.inp') # batch size
-    crucible_fraction=numpy.loadtxt('crucible.fraction.inp') # fraction of material left in the crucible during melting; 1st element is the expected quantity, 2nd and 3rd are the range for the true quantity
-    edge_time=numpy.loadtxt('edge.time.inp') # time elapsed on each edge transition
-    facility_operation=numpy.loadtxt('facility.operation.inp') # total time of facility operation; i.e., simulation time 
-    false_alarm_threshold=numpy.loadtxt('false.alarm.threshold.inp',usecols=[1]) # false alarm thresholds
-    inspection_time=numpy.loadtxt('inspection.time.inp',usecols=[1]) # time elapsed for each inspection
-    kmp_measurement_uncertainty=numpy.loadtxt('kmp.measurement.uncertainty.inp') # measurement uncertainty at each KMP
-    kmp_time=numpy.loadtxt('kmp.time.inp') # measurement time at each KMP
-    kmp_measurement_threshold=numpy.loadtxt('kmp.measurement.threshold.inp') # measurement threshold to trigger false alarms at each KMP
-    maximum_kmp=numpy.loadtxt('kmp.inp',dtype=int) # maximum number of KMPs
-    melter_failure_number=numpy.loadtxt('melter.failure.number.inp',dtype=int) # total number of possible melter failures
-    melter_failure_type=numpy.loadtxt('melter.failure.data.inp',usecols=[0],dtype=str) # type of melter failure
-    melter_failure_rate=numpy.loadtxt('melter.failure.data.inp',usecols=[1]) # corresponding melter failure rate
-    melter_failure_maintenance_time=numpy.loadtxt('melter.failure.data.inp',usecols=[2]) # time to repair each failure
-    melter_cleaning_time=numpy.loadtxt('melter.cleaning.time.inp') # time to clean the melter prior to equipment removal
-    process_time=numpy.loadtxt('process.time.inp',usecols=[1]) # time for each vertex to process material
-    weibull_beta_melter=numpy.loadtxt('weibull.beta.inp') # weibull distribution beta parameter for the melter
+    batch=numpy.loadtxt('batch.inp') #batch size
+    crucible_fraction=numpy.loadtxt('crucible.fraction.inp') #fraction of material left in the crucible during melting; 1st element is the expected quantity, 2nd and 3rd are the range for the true quantity
+    edge_transition=numpy.loadtxt('edge_transition\\edge.transition.inp') #time elapsed on each edge transition
+    facility_operation=numpy.loadtxt('facility.operation.inp') #total time of facility operation; i.e., simulation time 
+    false_alarm_threshold=numpy.loadtxt('false.alarm.threshold.inp',usecols=[1]) #false alarm thresholds
+    inspection_time=numpy.loadtxt('inspection.time.inp',usecols=[1]) #time elapsed for each inspection
+    kmp_data=numpy.loadtxt('kmps\\kmp.measurement.points.inp') #KMP data
+    melter_failure_number=numpy.loadtxt('melter.failure.number.inp',dtype=int) #total number of possible melter failures
+    melter_failure_type=numpy.loadtxt('melter.failure.data.inp',usecols=[0],dtype=str) #type of melter failure
+    melter_failure_rate=numpy.loadtxt('melter.failure.data.inp',usecols=[1]) #corresponding melter failure rate
+    melter_failure_maintenance_time=numpy.loadtxt('melter.failure.data.inp',usecols=[2]) #time to repair each failure
+    melter_cleaning_time=numpy.loadtxt('melter.cleaning.time.inp') #time to clean the melter prior to equipment removal
+    process_time=numpy.loadtxt('process.time.inp',usecols=[1]) #time for each vertex to process material
+    weibull_beta_melter=numpy.loadtxt('weibull.beta.inp') #weibull distribution beta parameter for the melter
 ###
     readme_input=open('readme.md').readlines()
 ###
@@ -232,21 +228,6 @@ def open_output_files(home_dir,output_data_dir):
     expected_processed_inventory_output=open('expected.processed.inventory.out','w+')
     measured_processed_inventory_output=open('measured.processed.inventory.out','w+')
     total_melter_failure_output=open('total.melter.failures.out','w+')
-    true_kmp0=open('true.kmp0.out','w+')
-    true_kmp1=open('true.kmp1.out','w+')
-    true_kmp2=open('true.kmp2.out','w+')
-    true_kmp3=open('true.kmp3.out','w+')
-    true_kmp4=open('true.kmp4.out','w+')
-    expected_kmp0=open('expected.kmp0.out','w+')
-    expected_kmp1=open('expected.kmp1.out','w+')
-    expected_kmp2=open('expected.kmp2.out','w+')
-    expected_kmp3=open('expected.kmp3.out','w+')
-    expected_kmp4=open('expected.kmp4.out','w+')
-    measured_kmp0=open('measured.kmp0.out','w+')
-    measured_kmp1=open('measured.kmp1.out','w+')
-    measured_kmp2=open('measured.kmp2.out','w+')
-    measured_kmp3=open('measured.kmp3.out','w+')
-    measured_kmp4=open('measured.kmp4.out','w+')
     true_heel=open('true.heel.out','w+')
     expected_heel=open('expected.heel.out','w+')
     measured_heel=open('measured.heel.out','w+')
@@ -320,7 +301,6 @@ def initialize_parameters(unprocessed_storage_inventory):
 #######
 def write_output(operation_time,failure_time,total_campaign,measured_storage_inventory,true_weight,expected_weight,measured_weight,true_muf,expected_muf,measured_muf,true_mufc,expected_mufc,measured_mufc,true_processed_inventory,expected_processed_inventory,measured_processed_inventory,total_melter_failure,measured_system_inventory,melter_process_counter,trimmer_process_counter,melter_probability_density_function_evaluate,melter_probability_density_function_failure_evaluate,melter_unreliability_function_evaluate,melter_unreliability_function_failure_evaluate,time_output,campaign_output,measured_storage_inventory_output,true_weight_output,expected_weight_output,measured_weight_output,true_muf_output,expected_muf_output,measured_muf_output,true_mufc_output,expected_mufc_output,measured_mufc_output,true_processed_inventory_output,expected_processed_inventory_output,measured_processed_inventory_output,total_melter_failure_output,measured_system_inventory_output,melter_process_counter_output,trimmer_process_counter_output,melter_probability_density_function_output,melter_unreliability_function_output):
 #######
-#
     time_output.write(str.format('%.4f'%operation_time)+'\t'+str.format('%.4f'%failure_time)+'\n')  
     campaign_output.write(str.format('%.4f'%operation_time)+'\t'+str.format('%i'%total_campaign)+'\n')
     measured_storage_inventory_output.write(str.format('%.4f'%operation_time)+'\t'+str.format('%.4f'%measured_storage_inventory)+'\n')
@@ -348,53 +328,6 @@ def write_output(operation_time,failure_time,total_campaign,measured_storage_inv
 ########################################################################
 #
 #
-#
-####### (e): KMP measurement
-# Conducts measurement at a KMP
-# The kmp_counter identifies which KMP parameters
-# These are delay time, threshold, measurement uncertainty
-# If there is a discrepancy with the measured weight and expected weight
-# Then the alarm is triggered
-# The measured quantities will change because each KMP has uncertainty
-###
-#
-###
-def kmp_measurement(operation_time,uncertainty,delay,threshold,true_quantity,expected_quantity,measured_inventory,measured_system_inventory,kmp_identifier):
-###
-    print 'Measurement event at KMP:',kmp_identifier
-    operation_time=operation_time+delay
-    measured_quantity=0
-###
-    if (kmp_identifier==0): #storage transfer
-        measured_quantity=true_quantity+uncertainty*numpy.random.randn()
-        measured_inventory=measured_inventory-measured_quantity
-        measured_system_inventory=measured_system_inventory+measured_quantity
-#
-        true_initial_inventory=true_quantity
-        expected_initial_inventory=expected_quantity
-        measured_initial_inventory=measured_quantity
-#
-    elif (kmp_identifier==1): #trimmer
-        measured_quantity=true_quantity+uncertainty*numpy.random.randn()
-#
-    elif (kmp_identifier==2): #product storage
-        measured_quantity=true_quantity+uncertainty*numpy.random.randn()
-        measured_inventory=measured_inventory+measured_quantity
-#
-    elif (kmp_identifier==3): #recycle transfer for failure
-        measured_quantity=true_quantity+uncertainty*numpy.random.randn()
-#
-    elif (kmp_identifier==-3): #transfer back from recycle to melter
-        measured_quantity=true_quantity+uncertainty*numpy.random.randn()
-# end if
-###
-    print 'Operation time','%.4f'%operation_time,'(d)','\n','True quantity','%.4f'%true_quantity,'(kg)','\n','Expected quantity','%.4f'%expected_quantity,'(kg)','\n','Measured quantity','%.4f'%measured_quantity,'(kg)','\n\n'
-###
-    if (kmp_identifier==0):
-        return(operation_time,measured_quantity,measured_inventory,true_initial_inventory,expected_initial_inventory,measured_initial_inventory,measured_system_inventory)
-    else:
-        return(operation_time,measured_quantity,measured_inventory)
-########################################################################
 #
 #
 #
@@ -523,32 +456,6 @@ def mass_balance(operation_time,delay,storage_inventory_start,true_storage_inven
 #
 #
 #
-####### (j): false alarm test
-# Tests for false alarm at inspection and KMP measurements
-###
-#
-###
-def false_alarm_test(threshold,false_alarm_counter,expected_quantity,measured_quantity):
-###
-    false_alarm=False
-    alarm_test=0
-    alarm_test=abs(measured_quantity-expected_quantity)
-    if (alarm_test>threshold):
-        false_alarm=True
-        false_alarm_counter=false_alarm_counter+1
-        print 'False alarm triggered','\n','Threshold','%.4f'%threshold,'Test','%.4f'%alarm_test,'Alarm count',false_alarm_counter,'\n\n'
-    elif (measured_quantity==expected_quantity):
-        print 'No false alarm','\n','Threshold','%.4f'%threshold,'Test','%.4f'%0,'Alarm count',false_alarm_counter,'\n\n'
-        
-    else:
-        print 'No false alarm','\n','Threshold','%.4f'%threshold,'Test','%.4f'%alarm_test,'Alarm count',false_alarm_counter,'\n\n'
-# end if
-###
-    return(false_alarm_counter,false_alarm,alarm_test)
-########################################################################
-#
-#
-#
 ####### (k): recycle storage
 # Metal alloy is removed from the melter and moved to recycle on failure
 ###
@@ -667,7 +574,6 @@ def reset_weight():
 #######
 def close_files(time_output,campaign_output,measured_storage_inventory_output,true_weight_output,expected_weight_output,measured_weight_output,true_muf_output,expected_muf_output,measured_muf_output,true_mufc_output,expected_mufc_output,measured_mufc_output,true_processed_inventory_output,expected_processed_inventory_output,measured_processed_inventory_output,total_melter_failure_output,true_kmp0,true_kmp1,true_kmp2,true_kmp3,true_kmp4,expected_kmp0,expected_kmp1,expected_kmp2,expected_kmp3,expected_kmp4,measured_kmp0,measured_kmp1,measured_kmp2,measured_kmp3,measured_kmp4,true_heel,expected_heel,measured_heel,measured_system_inventory_output,melter_process_counter_output,trimmer_process_counter_output):
 #######
-#
     time_output.close()
     campaign_output.close()
     measured_storage_inventory_output.close()
@@ -684,21 +590,6 @@ def close_files(time_output,campaign_output,measured_storage_inventory_output,tr
     expected_processed_inventory_output.close()
     measured_processed_inventory_output.close()
     total_melter_failure_output.close()
-    true_kmp0.close()
-    true_kmp1.close()
-    true_kmp2.close()
-    true_kmp3.close()
-    true_kmp4.close()
-    expected_kmp0.close()
-    expected_kmp1.close()
-    expected_kmp2.close()
-    expected_kmp3.close()
-    expected_kmp4.close()
-    measured_kmp0.close()
-    measured_kmp1.close()
-    measured_kmp2.close()
-    measured_kmp3.close()
-    measured_kmp4.close()
     true_heel.close()
     expected_heel.close()
     measured_heel.close()
@@ -758,52 +649,6 @@ def maintenance_melter(operation_time,delay,true_weight,expected_weight,measured
     return(operation_time,true_weight,expected_weight,measured_weight,accumulated_true_crucible,accumulated_expected_crucible,accumulated_measured_crucible,true_initial_inventory,expected_initial_inventory,measured_initial_inventory)
 ########################################################################
 #
-#
-#
-####### (u): KMP measurement recording write to file
-# Maintenance is performed on the equipment
-# Currently this is only for the melter
-###
-def kmp_write(operation_time,true_quantity,expected_quantity,measured_quantity,true_kmp,expected_kmp,measured_kmp):
-###
-    true_kmp.write(str.format('%.4f'%operation_time)+'\t'+str.format('%.4f'%true_quantity)+'\n')
-    expected_kmp.write(str.format('%.4f'%operation_time)+'\t'+str.format('%.4f'%expected_quantity)+'\n')
-    measured_kmp.write(str.format('%.4f'%operation_time)+'\t'+str.format('%.4f'%measured_quantity)+'\n')#
-###
-    return(true_kmp,expected_kmp,measured_kmp)
-########################################################################
-#
-#
-#
-####### (y): Weibull probability density function evaluation
-# Compute the probability density function [f(t)] for the weibull distribution
-###
-#
-def weibull_probability_density_function(time_domain,weibull_beta,weibull_eta):
-###
-    function_evaluate=(weibull_beta/weibull_eta)*((time_domain/weibull_eta)**(weibull_beta-1))*numpy.exp(-(time_domain/weibull_eta)**(weibull_beta))
-###
-    return(function_evaluate)
-########################################################################
-#
-#
-#
-####### (z): Weibull unreliability function evaluation
-# Compute the unreliability function [Q(t)]for the weibull distribution
-# This is also the cumulative density function [F(t)]
-###
-#
-def weibull_unreliability_function(time_domain,weibull_beta,weibull_eta):
-###
-    function_evaluate=1-numpy.exp(-(time_domain/weibull_eta)**(weibull_beta)) 
-###
-    return(function_evaluate)
-########################################################################
-#
-#
-#
-########################################################################
-#
-#      EOF
+# EOF
 #
 ########################################################################
